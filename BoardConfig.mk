fd99e1168f83a6a8dad8534e19d42d3933caac70
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-04 20:03:43 +0530

    More flags for smaller recovery.img

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,14 +72,17 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/twrp.fstab
 
 # TWRP specific flags
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 255
 
-# Exclude
+# Some flags to get smaller recovery.img
+TW_USE_TOOLBOX := true
+#TW_EXTRA_LANGUAGES := true
+TW_NO_EXFAT := true
+TW_EXCLUDE_SUPERSU := true
 TW_EXCLUDE_BASH := true
 TW_EXCLUDE_NANO := true
 TW_EXCLUDE_TZDATA := true
