217cc6168825241fc45518a91a97386d245bbc48
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-04 15:00:31 +0530

    Include brightness flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,9 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 255
 
 # Exclude
 TW_EXCLUDE_BASH := true
